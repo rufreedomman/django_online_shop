863c9c425d6c2dd6d1f28273e692d9da8536809d
rufreedomman <rufreedomman@gmail.com>
2022-05-23 15:27:15 +0300

    create: coupons app; add coupon model and register in admin

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -24,10 +24,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # My apps
     'shop.apps.ShopConfig',
     'cart.apps.CartConfig',
     'orders.apps.OrdersConfig',
     'payment.apps.PaymentConfig',
+    'coupons.apps.CouponsConfig',
 ]
 
 MIDDLEWARE = [
